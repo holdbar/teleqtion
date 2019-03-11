5c86f0f9292b564ea7baea88ad7a0ae89056c5c3
12bogdan03 <12bogdan03@gmail.com>
2019-03-11 23:07:47 +0200

    fix

--- backend/teleqtion/wsgi.py
+++ backend/teleqtion/wsgi.py
@@ -7,10 +7,11 @@ from django.conf import settings
 from decouple import config
 
 
-sys.path.append(os.path.join(settings.BASE_DIR, "apps"))
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE',
                       config('DJANGO_SETTINGS_MODULE',
                              default='teleqtion.settings.local'))
+
+sys.path.append(os.path.join(settings.BASE_DIR, "apps"))
+
 
 application = get_wsgi_application()
