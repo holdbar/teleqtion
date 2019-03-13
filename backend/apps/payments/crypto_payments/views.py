20d735e6109f79642bd4c1d742d76d7d21c598a4
12bogdan03 <12bogdan03@gmail.com>
2019-03-13 23:21:18 +0200

    fix

--- backend/apps/payments/crypto_payments/views.py
+++ backend/apps/payments/crypto_payments/views.py
@@ -119,7 +119,7 @@ class IpnView(views.APIView):
                     payment.status = Payment.PAYMENT_STATUS_TIMEOUT
                 else:
                     payment.amount_paid = Decimal(request.POST['received_amount'])
-                if payment.amount_paid >= payment.amount_original:
+                if payment.amount_paid >= payment.amount_original():
                     payment.status = Payment.PAYMENT_STATUS_PAID
                     payment.user.update_balance(payment.amount)
                 payment.save()
