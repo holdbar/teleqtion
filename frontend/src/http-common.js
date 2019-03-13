404011214c34a4d3d9ce5b8d16b464d68a69b46f
12bogdan03 <12bogdan03@gmail.com>
2019-03-13 11:20:00 +0200

    fix

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,12 +1,12 @@
 import axios from 'axios'
 
-// const BASE_URL = 'https://teleqtion.com/api/v1/';
-// export const LOGIN_URL = 'https://teleqtion.com/login/';
-const BASE_URL = 'http://localhost:8000/api/v1/';
-export const LOGIN_URL = 'http://localhost:8000/login/';
+const BASE_URL = 'https://teleqtion.com/api/v1/';
+export const LOGIN_URL = 'https://teleqtion.com/login/';
+// const BASE_URL = 'http://localhost:8000/api/v1/';
+// export const LOGIN_URL = 'http://localhost:8000/login/';
 
-// const token = localStorage.getItem('token');
-const token = '84008577a7cc4ef6188dca4e4ef7e3e0f8f48450';
+const token = localStorage.getItem('token');
+// const token = '84008577a7cc4ef6188dca4e4ef7e3e0f8f48450';
 if (!token) {
   window.location.replace(LOGIN_URL);
 }
